c9c87927b4e7f0e90f267e21f5d9cf6407812de0
Yuriy <150030@mail.ru>
2021-10-11 16:19:37 +1000

    v2

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@
 #include <string>
 #include <string.h>
 #include <bitset>
+#include <omp.h>
 
 #define L 24
 #define N 24
@@ -105,107 +106,147 @@ int main(int argc, char* argv[])
     }
 
 
-    /////////// import the system
-    PartArray sys;
-    sys.setInteractionRange(irange);
-
-    if(!strcmp(argv[1] + strlen(argv[1]) - strlen(".mfsys"), ".mfsys"))
-    {
-        sys.load(string(argv[1]));
-        sys.state.hardReset();
-    } else {
-        string tmp;
-        double a,b,c,d;
-
-        while (getline(f,tmp))
-        {
-            if (tmp.length()<4 || tmp[0] == '#')
-                continue;
-            else {
-                istringstream(tmp)>>a>>b>>c>>d;
-                Part p;
-                p.pos.setXYZ(a,b,0);
-                p.m.setXYZ(c,d,0);
-                sys.insert(p);
-            }
-        }
-    }
-    f.close();
-
-
-    if (dbg){
-        cout<<"file '"<<argv[1]<<"': imported "<<sys.size()<<" parts"<<endl;
-        cout<<"Energy: "<<sys.E()<<" range: All"<<endl;
-    }
-
-    if (sys.size()>64){
-        cout<<"System size is more than 64. It is impossible task! Exiting."<<endl;
-        return 0;
-    }
-
-    /////////// reserve the memory
-    const double divider=pow(10,precision);
-    const int memsize = (emax-emin)*divider+1;
-    unsigned long long * dos = new unsigned long long [memsize];
+    const double dividerGlob=pow(10,precision);
+    const int memsize = (emax-emin)*dividerGlob+1;
+    unsigned long long * dosGlob = new unsigned long long [memsize];
     for (unsigned i=0;i<memsize;++i){
-        dos[i]=0;
+        dosGlob[i]=0;
     }
 
-    /////////// enumerate the states
 
-    /**
-     * gray code iteration algorithm (from wiki): 
-     * https://en.wikipedia.org/wiki/Gray_code#Constructing_an_n-bit_Gray_code
-     * To construct the binary-reflected Gray code iteratively, at step 0 start with the 
-     * {\displaystyle \mathrm {code} _{0}=0}{\displaystyle \mathrm {code} _{0}=0}, 
-     * and at step {\displaystyle i>0}i>0 find the bit position of the least significant 
-     * 1 in the binary representation of {\displaystyle i}i and flip the bit at that 
-     * position in the previous code 
-     * {\displaystyle \mathrm {code} _{i-1}}\mathrm {code} _{i-1} 
-     * to get the next code 
-     * {\displaystyle \mathrm {code} _{i}}{\displaystyle \mathrm {code} _{i}}. 
-     * The bit positions start 0, 1, 0, 2, 0, 1, 0, 3,...
-     * */
+    #pragma omp parallel
+    {
+        int threadCount = omp_get_num_threads();        
+        #pragma omp for
+        for (int thread=0; thread<threadCount; ++thread)
+        {
+            /////////// import the system
+            PartArray sys;
+            sys.setInteractionRange(irange);
 
-    /*do{
-        cout<<sys.state.toString()<<" | "<<sys.E()<<endl;
-    } while(sys.state.next());*/
+            if(!strcmp(argv[1] + strlen(argv[1]) - strlen(".mfsys"), ".mfsys"))
+            {
+                sys.load(string(argv[1]));
+                sys.state.hardReset();
+            } else {
+                string tmp;
+                double a,b,c,d;
 
-    unsigned long long binState;
-    unsigned long long states=1;
-    states = states << (sys.size()-1);
-    unsigned partnum = 0;
-    unsigned long long every = states/100;
-    unsigned long long everyCounter = every;
-    unsigned percent = 0;
+                while (getline(f,tmp))
+                {
+                    if (tmp.length()<4 || tmp[0] == '#')
+                        continue;
+                    else {
+                        istringstream(tmp)>>a>>b>>c>>d;
+                        Part p;
+                        p.pos.setXYZ(a,b,0);
+                        p.m.setXYZ(c,d,0);
+                        sys.insert(p);
+                    }
+                }
+            }
+            f.close();
 
-    printf("# ");
-    for (binState=1;binState<=states;++binState){
-        if (sys.E()<emin)
-            dos[0]+=2;
-        else if (sys.E()>emax)
-            dos[memsize-1]+=2;
-        else
-            dos[int((sys.E()-emin)*divider)]+=2;
-        partnum = ffsll(binState)-1;// ffsll() - find first set bit in word
-        sys.parts[partnum]->rotate();
-        --everyCounter;
-        if (everyCounter==0){
-            everyCounter = every;
-            printf("%d%%..",percent);
-            fflush(stdout);
-            ++percent;
+
+            if (thread==0 && dbg){
+                cout<<"file '"<<argv[1]<<"': imported "<<sys.size()<<" parts"<<endl;
+                cout<<"Energy: "<<sys.E()<<" range: All"<<endl;
+            }
+
+            if (sys.size()>64){
+                cout<<"System size is more than 64. It is impossible task! Exiting."<<endl;
+            }
+
+            /////////// reserve the memory
+            const double divider=pow(10,precision);
+            unsigned long long * dos = new unsigned long long [memsize];
+            for (unsigned i=0;i<memsize;++i){
+                dos[i]=0;
+            }
+
+            /////////// enumerate the states
+
+            /**
+            * gray code iteration algorithm (from wiki): 
+            * https://en.wikipedia.org/wiki/Gray_code#Constructing_an_n-bit_Gray_code
+            * To construct the binary-reflected Gray code iteratively, at step 0 start with the 
+            * {\displaystyle \mathrm {code} _{0}=0}{\displaystyle \mathrm {code} _{0}=0}, 
+            * and at step {\displaystyle i>0}i>0 find the bit position of the least significant 
+            * 1 in the binary representation of {\displaystyle i}i and flip the bit at that 
+            * position in the previous code 
+            * {\displaystyle \mathrm {code} _{i-1}}\mathrm {code} _{i-1} 
+            * to get the next code 
+            * {\displaystyle \mathrm {code} _{i}}{\displaystyle \mathrm {code} _{i}}. 
+            * The bit positions start 0, 1, 0, 2, 0, 1, 0, 3,...
+            * */
+
+            /*do{
+                cout<<sys.state.toString()<<" | "<<sys.E()<<endl;
+            } while(sys.state.next());*/
+
+
+            unsigned long long totalStates=1;
+            totalStates = totalStates << (sys.size()-1);
+            unsigned long long statesPerThread = totalStates/threadCount;
+
+            unsigned long long stateFrom = statesPerThread*thread;
+            unsigned long long stateTo = stateFrom+statesPerThread;
+            if (thread==(threadCount-1)) stateTo = totalStates;
+
+            /*#pragma omp critical
+            {
+                cout<<thread<<" "<<stateFrom<<" "<<stateTo<<" "<<stateTo-stateFrom<<" "<<totalStates<<endl;
+            }*/
+            
+            unsigned long long binState;
+            unsigned partnum = 0;
+
+            //variables to write out percents
+            unsigned long long every = (stateTo-stateFrom)/100;
+            unsigned long long everyCounter = every;
+            unsigned percent = 0;
+
+            sys.state.fromString(std::bitset< 64 >( stateFrom ).to_string());
+
+            if (thread==0) cerr<<"# ";
+            for (binState=stateFrom+1;binState<=stateTo;++binState){
+                if (sys.E()<emin)
+                    dos[0]+=2;
+                else if (sys.E()>emax)
+                    dos[memsize-1]+=2;
+                else
+                    dos[int((sys.E()-emin)*divider)]+=2;
+                partnum = ffsll(binState)-1;// ffsll() - find first set bit in word
+                sys.parts[partnum]->rotate();
+                --everyCounter;
+                if (thread==0 && everyCounter==0){
+                    everyCounter = every;
+                    cerr<<percent<<"%..";
+                    cerr.flush();
+                    ++percent;
+                }
+            }
+            if (thread==0) cerr<<"\n";
+
+            //gather dos here
+            #pragma omp critical
+            for (unsigned i=0;i<memsize;++i){
+                if (dos[i]>0){
+                    dosGlob[i] += dos[i];
+                }
+            }
+            
+            delete[] dos;
         }
     }
-    printf("\n");
-
     /////////// print the dos
     for (unsigned i=0;i<memsize;++i){
-        if (dos[i]>0){
-            fprintf(ofile,"%f\t%d\n",(i/divider)+emin,dos[i]);
+        if (dosGlob[i]>0){
+            fprintf(ofile,"%f\t%d\n",(i/dividerGlob)+emin,dosGlob[i]);
         }
     }
 
-    delete[] dos;
+    delete[] dosGlob;
+
 
 }
